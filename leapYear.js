9c919fe646e2034ca90e04362c10781f8a6c7f52
imranhasme1wdc <imranhasmewdc.jm@gmail.com>
2022-10-26 09:13:32 +0600

    function with leapYear

--- leapYear.js
+++ leapYear.js
@@ -1,5 +1,6 @@
 //moulas uses================%
 
+
 const year = 4424;
 const remainder = year % 4;
 console.log(remainder == 0);
@@ -15,3 +16,26 @@ if(remainder1 == 0){
     console.log('your year is a leap Year');
 }
 else(console.log('your is is not leap year'))
+
+
+ 
+
+
+
+//with function uses-------------
+
+function isLeapYear (year){
+    const remainder2 = year % 4;
+    if(remainder2 == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const check2423 = isLeapYear(2423);
+console.log(check2423)
+
+
+//basic function-------------------
