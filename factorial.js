47d5ce0330663ce8ce410daa4090f6db5be6b39a
imranhasme1wdc <imranhasmewdc.jm@gmail.com>
2022-10-26 11:15:55 +0600

    for use in factorial

---
+++ factorial.js
@@ -0,0 +1,15 @@
+//when get operation in sum addion  , the index star [0] amd when it will be multiplication star[1]
+
+
+let factorial = 1;
+for(let i = 1; i <= 5; i++){
+   factorial = factorial* i;
+    console.log(i, factorial);
+}
+
+
+let fackto = 1;
+for(let i = 1; i <= 10; i++){
+   fackto = fackto * i
+    console.log(i,fackto)
+}
