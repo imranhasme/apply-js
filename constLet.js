792e1dec52161fc6fc8cc4ad47176ee498f72ba4
imranhasme1wdc <imranhasmewdc.jm@gmail.com>
2022-10-26 08:38:36 +0600

    const and let use

---
+++ constLet.js
@@ -0,0 +1,17 @@
+
+//when consdition will full fill
+
+var nameIs = "jabir"
+if(nameIs.length > 4){
+    nameIs = "juvu";
+}
+console.log(nameIs)
+
+
+
+//when variable declare constant----------
+
+const country = "Bangladesh"
+
+const name = "Imran"
+console.log(country)
