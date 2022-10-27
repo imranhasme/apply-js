e93dd2fbcb2b02040cd83aa752bab5439a5a2b31
Imran Hasme <108411987+imranhasme@users.noreply.github.com>
2022-10-27 16:49:14 +0600

    fibonacciSeriesRecursive

---
+++ fibonacciSeriesRecursive.js
@@ -0,0 +1,20 @@
+//[0,1,1,2,3,5,8,13,21,34]
+
+function fibonacci(n){
+    if(n==0){
+        return[0]
+    }
+    else if(n==1){
+        return[0,1]
+    }
+    else{
+        // calculate array nth element
+        let fibo = fibonacci(n-1);
+        let nextElement = fibo[n-1] + fibo[n -2];
+        fibo.push(nextElement);
+        return fibo;
+    }
+}
+
+let result = fibonacci(10);
+console.log(result);
